321932fe9e03fa800667bdd055edb3e5a57498f9
HariTTira <haritpao@gmail.com>
2024-08-06 19:48:01 +0700

    Adding component for home pags

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays, faUtensils } from "@fortawesome/free-solid-svg-icons";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./header.css";
+
+const Header = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <h1 className="headerTitle">
+          An Easy Way to Have a Meal
+        </h1>
+        <p className="headerDesc">
+          Find A nice Reastaurant With a Promotion!
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faUtensils} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where do you want to eat?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              dateFormat="MM/dd/yyyy"
+              className="headerDatePicker"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
