88a6a3fedb3b653ddb7b96bae7f94da76ebd98bc
HariTTira <haritpao@gmail.com>
2024-08-09 16:06:07 +0700

    Restaurant Detail and Booking Detail

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,18 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Restaurant from "./pages/RestaurantDe/Restaurant";
+import BookingPage from "./pages/bookingdetail/bookingde";
+import List from "./pages/list/List";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/restaurants" element={<List/>}/>
+        <Route path="/restaurant/:id" element={<Restaurant/>}/>
+        <Route path="/bookingdetail" element={<BookingPage/>}/>
       </Routes>
     </BrowserRouter>
   );
